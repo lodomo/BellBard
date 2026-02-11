fb1a230e96169888541319983f0f8c917ef3bf13
Lodomo <lodomo.dev@gmail.com>
2026-02-11 11:14:29 -0800

    Gitignore

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+sudo apt install pip -y
 sudo apt install python3-gunicorn -y
 sudo apt install python3-flask -y
+sudo apt install python3-toml -y
 sudo python3 -m pip install pygame-ce --break-system-packages
