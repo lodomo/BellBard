b7d3cf2e5c46d6bba86dbb736382df9b9159a41d
Lodomo <lodomo.dev@gmail.com>
2026-02-11 13:56:19 -0800

    Fixing install script

--- install.sh
+++ install.sh
@@ -4,4 +4,4 @@ sudo apt install pip -y
 sudo apt install python3-gunicorn -y
 sudo apt install python3-flask -y
 sudo apt install python3-toml -y
-sudo python3 -m pip install pygame-ce --break-system-packages
+sudo apt install python3-pygame -y
