f1346503bba34b37b4e403c2b50d1f42d0454d70
Lodomo <lodomo.dev@gmail.com>
2026-02-23 19:15:36 -0800

    Fixed redirect

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+sudo apt install neovim
+sudo apt install tmux
 sudo apt install pip -y
 sudo apt install python3-gunicorn -y
 sudo apt install python3-flask -y
@@ -8,9 +10,10 @@ sudo apt install python3-pygame -y
 sudo apt install gunicorn -y
 sudo apt-get install -y iptables-persistent
 
-sudo iptables -t nat -A PREROUTING -p tcp --dport 5000 -j REDIRECT --to-port 80
+sudo iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-port 5000
+
+# Save
 sudo netfilter-persistent save
 sudo iptables -t nat -L -n -v
-
 
 echo "Installation complete, run ./launch.sh to start the server"
